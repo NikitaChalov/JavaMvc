027235f2f3778dc130fd0214b8604b57b8e3ee5f
Nikita Chalov <nikitka20chalov@mail.ru>
2020-09-21 10:40:19 +0300

    Add java project

---
+++ start/src/main/java/org/chalov/start/HelloController.java
@@ -0,0 +1,14 @@
+package org.chalov.start;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class HelloController {
+    @RequestMapping("/")
+    public String home()
+    {
+        return "home";
+    }
+}
