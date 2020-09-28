d53ab708bd1f351671be606288b50893b284a993
Nikita Chalov <nikitka20chalov@mail.ru>
2020-09-28 12:04:49 +0300

    Добавил беспроводную передачу данных с предоставлением зависимостей объектам. Бины в прошлом коммите("Добавил беспроводную передачу данных")!

--- intro2/src/main/java/org/chalov/intro2/Controllers/HomeController.java
+++ intro2/src/main/java/org/chalov/intro2/Controllers/HomeController.java
@@ -5,21 +5,19 @@ import org.chalov.intro2.implementation.SenderImpl;
 import org.chalov.intro2.implementation.WirelessSenderImpl;
 import org.chalov.intro2.interfaces.Receiver;
 import org.chalov.intro2.interfaces.Sender;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    private final Receiver receiver;
-    private final Sender sender;
+    @Autowired
+    private org.springframework.web.context.WebApplicationContext context;
 
-    public HomeController() {
-        this.receiver = new ReceiverImpl();
-        this.sender = new WirelessSenderImpl(this.receiver);
-    }
     @RequestMapping(value= "/")
     public String home() {
-        return sender.sendMessage("Hello world");
+        Sender sender = context.getBean("sender", Sender.class);
+        return sender.sendMessage("My message");
     }
 }
