21c5a724b1220c5041cb4273a4b37bbe9f737193
Nikita Chalov <nikitka20chalov@mail.ru>
2020-09-28 13:02:23 +0300

    данные передаются в представление из test.jsp(не рабочий комит)

--- intro2/src/main/java/org/chalov/intro2/Controllers/HomeController.java
+++ intro2/src/main/java/org/chalov/intro2/Controllers/HomeController.java
@@ -4,6 +4,7 @@ import org.chalov.intro2.confings.JavaConfig;
 import org.chalov.intro2.interfaces.Sender;
 import org.chalov.intro2.implementation.WirelessSenderImpl;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +13,10 @@ public class HomeController {
     private org.springframework.context.annotation.AnnotationConfigApplicationContext context;
 
     @RequestMapping(value= "/")
-    public String home() {
+    public String home(Model model) {
         context = new AnnotationConfigApplicationContext(JavaConfig.class);
         Sender sender = context.getBean("WirelessSenderImpl", Sender.class);
-        return sender.sendMessage(" Message from java config");
+        model.addAttribute("msg", sender.sendMessage("Message from java config"));
+        return "test";
     }
 }
