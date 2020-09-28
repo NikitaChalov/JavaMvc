2fd5c0a374ce90699f42a9ecad92d8bb9e4b71ce
Nikita Chalov <nikitka20chalov@mail.ru>
2020-09-28 13:05:44 +0300

    приветствие по имени(не рабочий комит)

--- intro2/src/main/java/org/chalov/intro2/Controllers/HomeController.java
+++ intro2/src/main/java/org/chalov/intro2/Controllers/HomeController.java
@@ -6,6 +6,7 @@ import org.chalov.intro2.implementation.WirelessSenderImpl;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,10 +14,11 @@ public class HomeController {
     private org.springframework.context.annotation.AnnotationConfigApplicationContext context;
 
     @RequestMapping(value= "/")
-    public String home(Model model) {
+    public String home(@RequestParam(value = "name", required = false, defaultValue = "stranger") String name,
+                       Model model) {
         context = new AnnotationConfigApplicationContext(JavaConfig.class);
         Sender sender = context.getBean("WirelessSenderImpl", Sender.class);
-        model.addAttribute("msg", sender.sendMessage("Message from java config"));
+        model.addAttribute("msg", sender.sendMessage("Hello " + name + "!"));
         return "test";
     }
 }
---
+++ intro2/target/intro2/WEB-INF/test.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Nikita
+  Date: 9/28/2020
+  Time: 1:00 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Test</title>
+</head>
+<body>
+<p>{msg}</p>
+</body>
+</html>
