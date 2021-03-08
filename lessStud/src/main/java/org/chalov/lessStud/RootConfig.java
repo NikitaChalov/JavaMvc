cbf909111510e6b71af5752c61b72ebe1f41c515
Nikita Chalov <nikitka20chalov@mail.ru>
2021-03-08 15:56:23 +0300

    Added training Java-MVC project

--- lessStud/src/main/java/org/chalov/lessStud/RootConfig.java
+++
@@ -1,9 +0,0 @@
-package org.chalov.lessStud;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-    @Configuration
-    @EnableWebMvc
-    public class RootConfig {
-}
