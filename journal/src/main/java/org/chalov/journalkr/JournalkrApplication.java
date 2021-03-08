5684aa7bf4fc6a1e128abb29f8802a5efe1f25b8
Nikita Chalov <nikitka20chalov@mail.ru>
2021-03-08 14:39:18 +0300

    Added new java mvc project

---
+++ journal/src/main/java/org/chalov/journalkr/JournalkrApplication.java
@@ -0,0 +1,11 @@
+package org.chalov.journalkr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalkrApplication {
+    public static void main(String[] args){
+        SpringApplication.run(JournalkrApplication.class, args);
+    }
+}
---
+++ journal/src/main/java/org/chalov/journalkr/entity/Role.java
@@ -0,0 +1,5 @@
+package org.chalov.journalkr.entity;
+
+public enum Role {
+    ADMIN, STUDENT, TEACHER;
+}
